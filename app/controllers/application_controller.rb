6bf325a9fd66bea7f24e472b2e8bd4a4a64f5647
Jon Aziz <jonaziz@gmail.com>
2013-03-26 09:02:41 -0400

    Add session basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # Available to controllers (because they inherit from here)
+  def current_user
+  	# If the session contains the user_id with non-nil, we have ID of some user
+  	# Return the user object for ID
+  	
+  	# Using instance variable helps limit calls
+  	# "Or equals" means 
+  		# if nil, set it -- if not, return it
+  	if session[:user_id]
+    	@current_user ||= User.find(session[:user_id])
+    end
+  end
+
+  # Makes available to all views
+  helper_method :current_user
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,33 @@
 class SessionsController < ApplicationController
-	# login form
+	# Login form
 	def new
 
 	end
 
-	# starting session
-	## not a new thing in DB (actually cookie), but associates user with a session
+	# Starting session
+	## Not a new thing in DB (actually cookie), but associates user with a session
 	def create
+		user = User.find_by_email(params[:email])
+		# Equivalent: User.where(:email => params[:email]).first
+		## In theory, where could return multiple entries, but we have validation 
 
+		# Was user found? AND
+		if user && user.authenticate(params[:password])
+			# We're storying user.id with key :user_id, but you can store many
+			# different things. This gets encrypted and stored in the browser as
+			# a cookie. User.id is conventional (due to size limit of cookies).
+			session[:user_id] = user.id
+			redirect_to products_path, :notice => "Logged in!"
+		else
+			flash.now[:alert] = "Invalid email or password"
+			render "new"
+		end
 	end
 
-	# ending session
+	# Ending session
 	def destroy
-
+		session[:user_id] = nil
+		redirect_to products_path, :notice => "Logged out!"
 	end
 
 end
