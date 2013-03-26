0fa947bb2aa8abddf1e0e4a1c2c89e709762661c
Jon Aziz <jonaziz@gmail.com>
2013-03-26 08:17:16 -0400

    Add session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+	# login form
+	def new
+
+	end
+
+	# starting session
+	## not a new thing in DB (actually cookie), but associates user with a session
+	def create
+
+	end
+
+	# ending session
+	def destroy
+
+	end
+
+end
