21dd3fd55ab5d060c1facdd8cd6be89ae89d2744
Jon Aziz <jonaziz@gmail.com>
2013-03-26 08:05:25 -0400

    Clean up of code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   has_secure_password
-  # validates_confirmation_of :password
-  # validates_presence_of :password, :on => :create
-  # attr_reader :password
-  # include ActiveModel::SecurePassword::InstanceMethodsOnActivation
-
 
   validates :name, :email, :presence => true
   validates :email, :uniqueness => true
